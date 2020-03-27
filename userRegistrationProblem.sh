11514fb42775336ab5e9f05406301b8fcd0cd5de
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-27 19:57:47 +0530

    Added: Entered a valid first name .

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome to user registration problem."
+
+#constants
+VALIDNAMEPATTERN="^[A-Z][a-z]{2,}$"
+
+
+read -p "Enter first name:" firstName
+
+if [[ $firstName =~ $VALIDNAMEPATTERN ]]
+then
+	echo "Valid name."
+else
+	echo "Invalid name."
+fi
