8ec3c486231fa4339667804c04116689ce6ae449
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-28 00:03:10 +0530

    Added: valid pattern for email id.

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -4,9 +4,9 @@ echo "Welcome to user registration problem."
 
 #constants
 VALIDNAMEPATTERN="^[A-Z][a-z]{2,}$"
+EMAILPATTERN="^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$"
 
 read -p "Enter first name: " firstName
-
 if [[ $firstName =~ $VALIDNAMEPATTERN ]]
 then
 	echo "Valid firstname."
@@ -15,10 +15,17 @@ else
 fi
 
 read -p "Enter last name: " lastName
-
 if [[ $lastName =~ $VALIDNAMEPATTERN ]]
 then
 	echo "Valid lastname."
 else
 	echo "Invalid name."
+fi
+
+read -p "Enter email address: " emailId
+if [[ $emailId =~ $EMAILPATTERN ]]
+then
+	echo "Valid Email ID."
+else
+	echo "Invalid Email ID."
 fi
